2734142e260f6e4d804c809aa565ad3a37acc040
Shadi Hamdan <39621385+Shamdan17@users.noreply.github.com>
2020-06-21 14:25:04 +0300

    (#8) Setup Logger and Maven (#14)
    * Add gitignore to get rid of compiled files
    
    * Add gitattributes to make project language statistics more accurate
    
    * Convert to maven project and add log4j and its properties
    
    * fix minor issues
    
    * Delete .DS_Store
    
    delete extra file
    
    * cleanup old unused files
    
    * setup github workflow, addressing #1
    
    * setup github workflow, addressing #1
    
    * improve github workflow file
    
    * fix merge issues

---
+++ src/main/java/lookup/LookupTable.java
@@ -0,0 +1,51 @@
+package lookup;
+
+import skipnode.SkipNodeIdentity;
+
+public interface LookupTable {
+    SkipNodeIdentity EMPTY_NODE = new SkipNodeIdentity("EMPTY", -1);
+
+    /**
+     * Updates the left neighbor on the given level to be the node
+     * @param node Node to be put on the lookup table
+     * @param level The level on which to insert the node
+     * @return Replaced node
+     */
+    SkipNodeIdentity UpdateLeft(SkipNodeIdentity node, int level);
+
+    /**
+     * Updates the right neighbor on the given level to be the node
+     * @param node Node to be put on the lookup table
+     * @param level The level on which to insert the node
+     * @return Replaced node
+     */
+    SkipNodeIdentity UpdateRight(SkipNodeIdentity node, int level);
+
+    /**
+     * Returns the right neighbor on the given level
+     * @param level The level to get the node from
+     * @return The right neighbor on the given level
+     */
+    SkipNodeIdentity GetRight(int level);
+
+    /**
+     * Returns the left neighbor on the given level
+     * @param level The level to get the node from
+     * @return The left neighbor on the given level
+     */
+    SkipNodeIdentity GetLeft(int level);
+
+    /**
+     * Remove the left neighbor on the given level
+     * @param level The level from which to remove the left neighbor
+     * @return Removed node
+     */
+    SkipNodeIdentity RemoveLeft(int level);
+
+    /**
+     * Remove the right neighbor on the given level
+     * @param level The level from which to remove the right neighbor
+     * @return Removed node
+     */
+    SkipNodeIdentity RemoveRight(int level);
+}
