7e9c23665b3a8f217eb1feec945efd8b9b53d2a7
yhassanzadeh13@ku.edu.tr <yhassanzadeh13@ku.edu.tr>
2021-01-25 06:15:16 -0800

    adds underlay test case

---
+++ src/test/java/underlay/UnderlayTestCase.java
@@ -0,0 +1,10 @@
+package underlay;
+
+public class UnderlayTestCase
+{
+    private static final int LOCAL_PORT = 9090;
+    private static final int REMOTE_PORT = 9091;
+
+    private static Underlay localUnderlay;
+    private static Underlay remoteUnderlay;
+}
