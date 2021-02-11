d87cb98850bc049ad15ac139c6c4e46382faf216
yhassanzadeh13@ku.edu.tr <yhassanzadeh13@ku.edu.tr>
2021-02-10 20:24:31 -0800

    adds lock manager

--- src/main/java/skipnode/LockManager.java
+++ src/main/java/skipnode/LockManager.java
@@ -1,2 +1,32 @@
-package skipnode;public class LockManager {
+package skipnode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class LockManager {
+    private LinkedBlockingDeque<InsertionLock.NeighborInstance> ownedLocks;
+
+    public LockManager() {
+        this.ownedLocks = new LinkedBlockingDeque<>();
+    }
+
+    public void addLock(SkipNodeIdentity identity, int level){
+        this.ownedLocks.add(new InsertionLock.NeighborInstance(identity, level));
+    }
+
+    public List<InsertionLock.NeighborInstance> releaseAll(){
+        List<InsertionLock.NeighborInstance> toRelease = new ArrayList<>();
+        ownedLocks.drainTo(toRelease);
+        return toRelease;
+    }
+
+    @Override
+    public String toString() {
+        String str = "";
+        for(InsertionLock.NeighborInstance n: ownedLocks){
+            str = " " + n.node.getNumID();
+        }
+        return str;
+    }
 }
