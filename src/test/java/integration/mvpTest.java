3386ac6470b611a09f3ae9bc8721960a3ac8f3c9
Bulut Bulgu <bbulgudacity@gmail.com>
2021-08-06 18:17:35 +0300

    Fix MVPTest

--- src/test/java/integration/mvpTest.java
+++ src/test/java/integration/mvpTest.java
@@ -1,25 +1,10 @@
 package integration;
 
 
-/*
+/**
 
-
-To establish a decentralized (and not local) Skip Graph overlay of 32 nodes and test for full connectivity over each node,
+The goal of the mvpTest is to establish a decentralized Skip Graph overlay of 32 nodes and test for full connectivity over each node,
  i.e., each node should be able to query every other node by both name and numerical IDs and get the correct response.
-
-The scenario should involve the first node creating itself through SkipNode constructor, then the rest of the nodes concurrently
-create themselves and try inserting in the skip graph using SkipNode.insert method by giving the first node as the introducer.
- **Note: for this we need to override another version of SkipNode constructor that does not require the LookupTable:
-
-public SkipNode(SkipNodeIdentity snID)
-
-We then run tests that each node queries a search for num ID of every other node concurrently and evaluate that the result is correct.
- We then do the same test for search for name ID.
-
-Note that in developing this issue we can borrow a lot of test logics from the already provided concurrentInsertion, searchByNameID,
-and searchByNumID. However, we need to be mindful that these tests are using LocalSkipGraph, however, we should have
- separate SkipNodes communicating with each other imitating a decentralized overlay. We can still keep the SkipNodes in
- an array or array list, but must not enforce any centralized behavior.
 
  */
 
@@ -34,16 +19,13 @@ import skipnode.SkipNode;
 import skipnode.SkipNodeIdentity;
 import underlay.Underlay;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static misc.LocalSkipGraph.prependToLength;
 
 public class mvpTest {
-    static int STARTING_PORT = 8080;
+    static int STARTING_PORT = 4444;
     static int NODES = 32;
 
     @Test
@@ -94,19 +76,18 @@ public class mvpTest {
             skipNodes.get(i).setMiddleLayer(middleLayer);
         }
 
-        SkipNode initiator = new SkipNode(identities.get(0), nameIDSize);
+        skipNodes.get(0).insert(null, -1);
 
         Thread[] threads = new Thread[NODES-1];
-        System.out.println("testing");
-
         // Construct the threads.
         for(int i = 1; i <= threads.length; i++) {
-            final SkipNode node = skipNodes.get(i);
+            final SkipNode node  = skipNodes.get(i);
+            Random rand = new Random();
+            final SkipNode introducer = skipNodes.get((int)(Math.random() * i));
             threads[i-1] = new Thread(() -> {
-                node.insert(initiator.getIdentity().getAddress(), initiator.getIdentity().getPort());
+                node.insert(introducer.getIdentity().getAddress(), introducer.getIdentity().getPort());
             });
         }
-
         // Initiate the insertions.
         for(Thread t : threads) t.start();
         // Wait for the insertions to complete.
@@ -119,13 +100,11 @@ public class mvpTest {
             }
         }
 
-        System.out.println("testing insertions 1");
-
         Thread[] searchThreads = new Thread[NODES * NODES];
-        for(int i = 0; i < 1; i++) {
+        for(int i = 0; i < NODES; i++) {
             // Choose the searcher.
             final SkipNode searcher = skipNodes.get(i);
-            for(int j = 0; j < 1; j++) {
+            for(int j = 0; j < NODES; j++) {
                 // Choose the target.
                 final SkipNode target = skipNodes.get(j);
                 searchThreads[i + NODES * j] = new Thread(() -> {
