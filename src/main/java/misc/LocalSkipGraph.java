8abcd1cd1b20ddbda4484da95d6ca215508ee2a5
bbulgu <bbulgu16@ku.edu.tr>
2021-08-18 22:50:02 +0300

    Update LocalSkipGraph.java

--- src/main/java/misc/LocalSkipGraph.java
+++ src/main/java/misc/LocalSkipGraph.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import lookup.ConcurrentLookupTable;
+import lookup.LookupTable;
 import model.NameId;
 import skipnode.SkipNode;
 import skipnode.SkipNodeIdentity;
