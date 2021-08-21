fbb72bc0e206099b8a6fb2b0e0ebf451d381b19b
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2021-08-21 06:09:18 -0700

    adds javadoc

--- src/main/java/misc/Utils.java
+++ src/main/java/misc/Utils.java
@@ -1,16 +1,33 @@
 package misc;
 
 import java.util.AbstractList;
-import java.util.Collection;
 import java.util.Random;
 
+/**
+ * General utilities for codebase.
+ */
 public class Utils {
-  public static Object randomIndex(AbstractList list, Random random){
+  /**
+   * Returns object of a random index in range of [0, limit] from the abstract list.
+   *
+   * @param list input list.
+   * @param random random generator.
+   * @return element of a random index from the list.
+   */
+  public static Object randomIndex(AbstractList list, Random random) {
     return list.get((Math.abs(random.nextInt()) % list.size()));
   }
 
-  public static Object randomIndex(AbstractList list, Random random, int limit){
-    if(limit <= 1) {
+  /**
+   * Returns object of a random index from the abstract list.
+   *
+   * @param list input list.
+   * @param random random generator.
+   * @param limit maximum index can be returned exclusive.
+   * @return element of a random index in range of [0, limit] from the list.
+   */
+  public static Object randomIndex(AbstractList list, Random random, int limit) {
+    if (limit <= 1) {
       return list.get(0);
     }
     return list.get((Math.abs(random.nextInt()) % limit));
