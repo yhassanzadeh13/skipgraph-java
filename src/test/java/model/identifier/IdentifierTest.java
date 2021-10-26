f1893c656d7ad9640b32980115173d8be7991f7b
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2021-10-26 06:45:16 -0700

    develops identifier create test

--- src/test/java/model/identifier/IdentifierTest.java
+++ src/test/java/model/identifier/IdentifierTest.java
@@ -1,5 +1,24 @@
 package model.identifier;
 
-public class Identifier {
+import io.ipfs.multibase.Multibase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import unittest.Fixtures;
 
+public class IdentifierTest {
+
+  @Test
+  void TestIdentifier_Create(){
+    byte[] bytes = Fixtures.ByteArrayFixture(SkipGraph.IDENTIFIER_SIZE);
+    Identifier id = new Identifier(bytes);
+
+    Assertions.assertArrayEquals(bytes, id.getBytes());
+
+    String idStr = Multibase.encode(Multibase.Base.Base58BTC, bytes);
+    Assertions.assertEquals(id.toString(), idStr);
+
+    // since identifiers are encoded using Base58BTC, all of them must
+    // start with character `z`.
+    Assertions.assertEquals('z', idStr.charAt(0));
+  }
 }
