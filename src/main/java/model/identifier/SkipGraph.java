5660918015752bcaf2e61ca3d8d672a1954f6895
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2021-10-26 06:45:38 -0700

    refactors identifier size

--- src/main/java/model/identifier/SkipGraph.java
+++ src/main/java/model/identifier/SkipGraph.java
@@ -3,6 +3,8 @@ package model.identifier;
 public class SkipGraph {
   /**
    * Size of name ID and numerical ID in bits.
+   *
+   * It is set to 32 bytes which corresponds to 256 bits.
    */
-  public final static int IDENTIFIER_SIZE = 10;
+  public final static int IDENTIFIER_SIZE = 32;
 }
