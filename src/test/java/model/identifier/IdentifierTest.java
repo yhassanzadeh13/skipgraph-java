c33ccda6fc77584b3d1f7b2514146316bddc03f0
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2021-10-26 06:59:40 -0700

    adds illegitimate size test

--- src/test/java/model/identifier/IdentifierTest.java
+++ src/test/java/model/identifier/IdentifierTest.java
@@ -3,10 +3,14 @@ package model.identifier;
 import io.ipfs.multibase.Multibase;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import unittest.Fixtures;
 
 public class IdentifierTest {
 
+  /**
+   * Asserts Identifier constructor happy path as well as its getters. 
+   */
   @Test
   void TestIdentifier_Create(){
     byte[] bytes = Fixtures.ByteArrayFixture(SkipGraph.IDENTIFIER_SIZE);
@@ -20,5 +24,19 @@ public class IdentifierTest {
     // since identifiers are encoded using Base58BTC, all of them must
     // start with character `z`.
     Assertions.assertEquals('z', idStr.charAt(0));
+  }
+
+  /**
+   * Asserts Identifier constructor returns an illegal argument exception if input bytes size
+   * is not exactly equal to skip graph identifier size.
+   */
+  @Test
+  void TestIdentifier_IllegitimateSize(){
+    byte[] smallerBytes = Fixtures.ByteArrayFixture(SkipGraph.IDENTIFIER_SIZE - 1);
+    Assertions.assertThrows(IllegalArgumentException.class, () -> new Identifier(smallerBytes));
+
+    byte[] biggerBytes = Fixtures.ByteArrayFixture(SkipGraph.IDENTIFIER_SIZE + 1);
+    Assertions.assertThrows(IllegalArgumentException.class, () -> new Identifier(biggerBytes));
+
   }
 }
