4945f6bbcec4da8987754965bb750166be9f11c6
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2021-10-30 06:35:17 -0700

    adds max and min bytes fixtures

--- src/test/java/unittest/Fixtures.java
+++ src/test/java/unittest/Fixtures.java
@@ -1,5 +1,6 @@
 package unittest;
 
+import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -16,6 +17,28 @@ public class Fixtures {
   public static byte[] ByteArrayFixture(int length){
     byte[] arr = new byte[length];
     random.nextBytes(arr);
+    return arr;
+  }
+
+  /**
+   * Generates a byte array with all values at minimum byte value, i.e., -127.
+   * @param length length of byte array.
+   * @return byte array.
+   */
+  public static byte[] MinByteArrayFixture(int length){
+    byte[] arr = new byte[length];
+    Arrays.fill(arr, Byte.MIN_VALUE);
+    return arr;
+  }
+
+  /**
+   * Generates a byte array with all values at maximum byte value, i.e., 128.
+   * @param length length of byte array.
+   * @return byte array.
+   */
+  public static byte[] MaxByteArrayFixture(int length){
+    byte[] arr = new byte[length];
+    Arrays.fill(arr, Byte.MAX_VALUE);
     return arr;
   }
 
