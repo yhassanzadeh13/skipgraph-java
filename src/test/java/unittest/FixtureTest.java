17ae0db9f2a55ca764ab584e33d1c2914333484d
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2021-11-02 06:49:24 -0700

    develops test byte array fixture

--- src/test/java/unittest/FixtureTest.java
+++ src/test/java/unittest/FixtureTest.java
@@ -1,2 +1,15 @@
-package unittest;public class FixtureTest {
+package unittest;
+
+import misc.Utils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FixtureTest {
+  @Test
+  public void TestByteArrayFixture_OneBitPrefix(){
+    byte[] onePrefix = Fixtures.ByteArrayFixture("1", 10);
+    Assertions.assertEquals(10, onePrefix.length);
+    System.out.println(Utils.toBinaryRepresentation(onePrefix[0]));
+    Assertions.assertTrue(Utils.toBinaryRepresentation(onePrefix[0]).startsWith("1"));
+  }
 }
