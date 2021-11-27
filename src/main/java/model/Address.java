7eaf7cb8ca386d28c47d518b89e695a8131b6d32
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2021-11-27 06:16:30 -0800

    adds network address model

--- src/main/java/model/Address.java
+++ src/main/java/model/Address.java
@@ -1,2 +1,37 @@
-package model;public class address {
+package model;
+
+import java.util.Objects;
+
+/**
+ * Represents the network address of a skip graph node.
+ */
+public class Address {
+  private final String ip;
+  private final int port;
+
+  public Address(String ip, int port) {
+    this.ip = ip;
+    this.port = port;
+  }
+
+  public String getIp() {
+    return ip;
+  }
+
+  public int getPort() {
+    return port;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof Address)) return false;
+    Address address = (Address) o;
+    return getPort() == address.getPort() && getIp().equals(address.getIp());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getIp(), getPort());
+  }
 }
